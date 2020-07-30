6082aa3e98064e1bb1aa2773bad61ab40e4abe28
Richard Bennett <inbox@richardbennett.dev>
2020-07-30 20:09:45 +0000

    Adds unique keys to tenant records.
    When we push this information into dependent systems, this tenant_key may be used to identify the tenant record within the dependent information.

--- urp_console/urp_console/doctype/urp_customer/urp_customer.py
+++ urp_console/urp_console/doctype/urp_customer/urp_customer.py
@@ -3,11 +3,28 @@
 # For license information, please see license.txt
 
 from __future__ import unicode_literals
-# import frappe
+import frappe
 from frappe.model.document import Document
 import random
 import string
 
 class URPCustomer(Document):
-	pass
+
+	def validate(self):
+		#frappe.throw(str(len(self.licenses_ref)))
+
+		for tenant in self.tenants_ref:
+			if not tenant.tenant_key:
+				tenant.tenant_key = get_random_alphanumeric_string(10)
+
+	def on_update(self):
+		# licenses_ref
+		pass
+
+
 	# def get_random_alphanumeric_string(length):
+
+def get_random_alphanumeric_string(length):
+	letters_and_digits = string.ascii_letters + string.digits
+	result_str = ''.join((random.choice(letters_and_digits) for i in range(length)))
+	return result_str
